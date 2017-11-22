6844916cb4bb34c007a2097b46f9434dbbdf4259
Richard Haeser <richard@maxserv.com>
2017-11-22 21:33:42 +0100

    [TASK] Changed TCA configuration for SEO title and canonical URL

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -9,8 +9,32 @@ $temporaryColumns = [
         'exclude' => 1,
         'label' => $ll.'pages.seo_title',
         'config' => [
-            'type' => 'input'
+            'type' => 'input',
+            'eval' => 'trim',
+            'max' => 255,
+            'size' => 50
         ],
+    ],
+    'canonical_url' => [
+        'exclude' => 1,
+        'label' => $ll.'pages.canonical_url',
+        'config' => [
+            'type' => 'input',
+            'renderType' => 'inputLink',
+            'size' => 50,
+            'max' => 1024,
+            'eval' => 'trim',
+            'fieldControl' => [
+                'linkPopup' => [
+                    'options' => [
+                        'title' => $ll.'formlabel.canonical_url',
+                        'blindLinkOptions' => 'file,mail,spec,folder',
+                        'blindLinkFields' => 'class,params,target,title'
+                    ],
+                ],
+            ],
+            'softref' => 'typolink'
+        ]
     ]
 ];
 
@@ -18,9 +42,19 @@ $temporaryColumns = [
     'pages',
     $temporaryColumns
 );
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
+    'pages',
+    'metadata',
+    '
+    --linebreak--, seo_title,
+    --linebreak--, canonical_url,
+    '
+);
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
     'pages',
-    '--div--;' . $ll . 'pages.tabs.seo, seo_title',
+    '--div--;' . $ll . 'pages.tabs.seo,--palette--;' . $ll . 'pages.palettes.metadata;metadata',
     '',
     'after:subtitle'
 );
---
+++ ext_tables.sql
@@ -0,0 +1,7 @@
+#
+# Table structure for table 'pages'
+#
+CREATE TABLE pages (
+	seo_title varchar(255) DEFAULT '' NOT NULL,
+	canonical_url varchar(1024) DEFAULT '' NOT NULL,
+);
