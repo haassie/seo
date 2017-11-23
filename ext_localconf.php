0199ff5bae8d24be4e4cffab9cf953057198fb0d
Georg Ringer <georg.ringer@gmail.com>
2017-11-23 11:59:45 +0100

    [TASK] Hide not needed fields by using TSconfig

---
+++ ext_localconf.php
@@ -0,0 +1,5 @@
+<?php
+
+defined('TYPO3_MODE') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:core_seo/Configuration/TSconfig/PageTSconfig.typoscript">');
